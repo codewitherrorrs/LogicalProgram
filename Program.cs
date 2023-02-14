a8c29acfb4adacd8011fb2d771ab075f33e74507
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-14 13:11:10 +0530

    Add : Coupon Generation Program

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Program
+{
+    public class logical
+    {
+        public static void Main(string[] args)
+        {
+            int first=0;
+            int temp;
+            Console.WriteLine("how many coupon number you required ?");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("genreted coupon number");
+
+            for (int i = 0; i < Number; i++)
+            {
+                Random number = new Random();
+                int coupon = number.Next(10000, 100000);
+
+                Console.WriteLine("Coupon : "+coupon);
+                Console.ReadLine();
+                temp = coupon;
+                first = temp;
+
+                if (first != temp)
+                {
+                    Console.WriteLine("Coupon Are same");
+                }
+                else
+                {
+                    Console.WriteLine("Coupon are Different");
+                }
+                
+            }
+        }
+    }
+}
